4e974c5d42bd7c4a84a20fe4930e59394d984d47
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-01-21 14:52:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Students from "./components/Students";
+import StudentForm from "./components/StudentForm";
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/*<p>Here is some text yippee!</p>*/}
+
+        <BrowserRouter>
+            <Routes>
+                <Route path = "/students" element ={<Students />}></Route>
+                <Route path="/studentForm" element ={<StudentForm />}></Route>
+            </Routes>
+        </BrowserRouter>
+        <h1>Demo Slices! </h1>
+
+
     </div>
   );
 }
